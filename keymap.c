169fbcbdcec69cdf62e2eaa91839ff7ec07db21a
Daniel Alvarez <elquimeras@gmail.com>
2022-07-07 13:48:12 -0400

    Add backslash to keymap

--- keymap.c
+++ keymap.c
@@ -54,6 +54,7 @@ enum {
   TD_BRKA,
   TD_BRKB,
   TD_QSTN,
+  TD_SLASH,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -87,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
   TD(TD_CAPLOCK),  KC_LEFT,  KC_DOWN,  KC_RGHT,  MACRO1,  MACRO2,              KC_MINS, KC_EQL, RALT(KC_LBRC),  RALT(KC_RBRC), KC_PIPE, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   LALT(KC_2), LSFT(KC_0), LSFT(KC_7), KC_LBRC, XXXXXXX, MACRO3,              TD(TD_QSTN), KC_PLUS, RALT(KC_QUOT), RALT(KC_NUHS), KC_BSLS, KC_ENT,
+   LALT(KC_2), LSFT(KC_0), TD(TD_SLASH), KC_LBRC, XXXXXXX, MACRO3,              TD(TD_QSTN), KC_PLUS, RALT(KC_QUOT), RALT(KC_NUHS), KC_BSLS, KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             KC_LGUI, LOWER, KC_SPC,    KC_SPC, KC_TRNS, KC_RALT
                                       //|--------------------------|  |--------------------------|
@@ -121,6 +122,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_BRKA] = ACTION_TAP_DANCE_DOUBLE(RALT(KC_LBRC), KC_GRV),
     [TD_BRKB] = ACTION_TAP_DANCE_DOUBLE(RALT(KC_RBRC), LSFT(KC_GRV)),
     [TD_QSTN] = ACTION_TAP_DANCE_DOUBLE(KC_UNDS, LSFT(KC_EQL)),
+    [TD_SLASH] = ACTION_TAP_DANCE_DOUBLE(LSFT(KC_7), RALT(KC_NUBS))
 };
 
 int RGB_current_mode;
